17a691d0975fd00c5aa387dc6873b90c5bdce57e
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-29 23:55:49 +0200

    test: write unit test for the max function

---
+++ src/tests/intro.test.js
@@ -0,0 +1,20 @@
+import { describe, test, it, expect } from "vitest";
+import { max } from "../intro";
+
+describe("max", () => {
+  it("should return the first argument if it is greater", () => {
+    // Arrange: Set up test environment including necessary data and configuration
+    // Act: Perform the action we want to test
+    // Assert: Check the outcome to see if the result matches our expectations
+
+    expect(max(2, 1)).toBe(2);
+  });
+
+  it("should return the second argument if is is greater", () => {
+    expect(max(3, 5)).toBe(5);
+  });
+
+  it("should return the first argument if both arguments are equal", () => {
+    expect(max(3, 3)).toBe(3);
+  });
+});
