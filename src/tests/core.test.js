a52d7f0bdfce32d9a599cc6277b99c0f4fb964e9
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-03 22:46:00 +0200

    feat: add boundary testing

--- src/tests/core.test.js
+++ src/tests/core.test.js
@@ -1,5 +1,10 @@
 import { it, expect, describe } from 'vitest';
-import { calculateDiscount, getCoupons, validateUserInput } from '../core';
+import {
+  calculateDiscount,
+  getCoupons,
+  isPriceInRange,
+  validateUserInput,
+} from '../core';
 
 describe('test suite', () => {
   it('test case', () => {
@@ -92,7 +97,23 @@ describe('validate user input', () => {
   });
 
   it('should return an error if both username and age are invalid', () => {
-    expect(validateUserInput("", 0)).toMatch(/Invalid username/i);
-    expect(validateUserInput("", 0)).toMatch(/Invalid age/i);
-  })
+    expect(validateUserInput('', 0)).toMatch(/Invalid username/i);
+    expect(validateUserInput('', 0)).toMatch(/Invalid age/i);
+  });
+});
+
+describe('isPriceinRange', () => {
+  it('should return false when the price is outside the range', () => {
+    expect(isPriceInRange(-10, 0, 100)).toBe(false);
+    expect(isPriceInRange(200, 0, 100)).toBe(false);
+  });
+
+  it('should return true when the price is equal to the min or to the max', () => {
+    expect(isPriceInRange(0, 0, 10)).toBe(true);
+    expect(isPriceInRange(100, 0, 100)).toBe(true);
+  });
+
+  it('should return true if the price is within the range', () => {
+    expect(isPriceInRange(50, 30, 100)).toBe(true);
+  });
 });
