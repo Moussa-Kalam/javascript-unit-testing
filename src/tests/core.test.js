bdf04c1e90881059ed6ba3de8467dfa9c7a60e55
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-03 22:31:51 +0200

    test: validate test input

--- src/tests/core.test.js
+++ src/tests/core.test.js
@@ -1,31 +1,98 @@
-import { it, expect, describe } from "vitest";
-import { getCoupons } from "../core";
+import { it, expect, describe } from 'vitest';
+import { calculateDiscount, getCoupons, validateUserInput } from '../core';
 
-describe("getCoupons", () => {
-  it("should return an array of coupons", () => {
+describe('test suite', () => {
+  it('test case', () => {
+    const result = 'The user was not found!';
+
+    expect(result).toBeDefined();
+    expect(result).toMatch('found');
+  });
+});
+
+describe('getCoupons', () => {
+  it('should return an array of coupons', () => {
     const coupons = getCoupons();
     expect(Array.isArray(coupons)).toBe(true);
     expect(coupons.length).toBeGreaterThan(0);
   });
 
-  it("should return an arry with valid coupon codes", () => {
+  it('should return an array with valid coupon codes', () => {
     const coupons = getCoupons();
 
     coupons.forEach((coupon) => {
-      expect(coupon).toHaveProperty("code");
-      expect(typeof coupon.code).toBe("string");
+      expect(coupon).toHaveProperty('code');
+      expect(typeof coupon.code).toBe('string');
       expect(coupon.code).toBeTruthy();
     });
   });
 
-  it("should return an array with valid actions", () => {
+  it('should return an array with valid discounts', () => {
     const coupons = getCoupons();
 
     coupons.forEach((coupon) => {
-      expect(coupon).toHaveProperty("discount");
-      expect(typeof coupon.discount).toBe("number");
+      expect(coupon).toHaveProperty('discount');
+      expect(typeof coupon.discount).toBe('number');
       expect(coupon.discount).toBeGreaterThan(0);
       expect(coupon.discount).toBeLessThan(1);
     });
   });
+});
+
+describe('calculate discounts', () => {
+  it('should return discounted price if given a valid code', () => {
+    expect(calculateDiscount(10, 'SAVE10')).toBe(9);
+    expect(calculateDiscount(10, 'SAVE20')).toBe(8);
+  });
+
+  it('should handle non-numeric price', () => {
+    expect(calculateDiscount('10', 'SAVE10')).toMatch(/invalid/i);
+  });
+
+  it('should handle negative price', () => {
+    expect(calculateDiscount(-10, 'SAVE10')).toMatch(/invalid/i);
+  });
+
+  it('should handle non-string discount code', () => {
+    expect(calculateDiscount(10, 34)).toMatch(/invalid/i);
+  });
+
+  it('should handle non-valid discount code', () => {
+    expect(calculateDiscount(10, 'SAVE30')).toBe(10);
+  });
+});
+
+describe('validate user input', () => {
+  it('should return validation successful if given valid input', () => {
+    expect(validateUserInput('Moussa', 25)).toMatch(/success/i);
+  });
+
+  it('should return an error if username is not a string', () => {
+    expect(validateUserInput(13, 25)).toMatch(/invalid/i);
+  });
+
+  it('should return an error if username is less than 3 characters', () => {
+    expect(validateUserInput('Mo', 25)).toMatch(/invalid/i);
+  });
+
+  it('should return an error if username is more than 255 characters', () => {
+    expect(validateUserInput('m'.repeat(256), 25)).toMatch(/invalid/i);
+  });
+
+  it('should return an error if age is not a number', () => {
+    expect(validateUserInput('Moussa', '34')).toMatch(/invalid/i);
+  });
+
+  it('should return an error if age is less than 18', () => {
+    expect(validateUserInput('Moussa', 16)).toMatch(/invalid/i);
+  });
+
+  it('should return an error if age is greater than 100', () => {
+    expect(validateUserInput('Moussa', 101)).toMatch(/invalid/i);
+  });
+
+  it('should return an error if both username and age are invalid', () => {
+    expect(validateUserInput("", 0)).toMatch(/Invalid username/i);
+    expect(validateUserInput("", 0)).toMatch(/Invalid age/i);
+  })
 });
--- src/tests/intro.test.js
+++ src/tests/intro.test.js
@@ -1,8 +1,8 @@
-import { describe, test, it, expect } from "vitest";
-import { factorial, fizzBuzz, max } from "../intro";
+import { describe, test, it, expect } from 'vitest';
+import { factorial, fizzBuzz, max } from '../intro';
 
-describe("max", () => {
-  it("should return the first argument if it is greater", () => {
+describe('max', () => {
+  it('should return the first argument if it is greater', () => {
     // Arrange: Set up test environment including necessary data and configuration
     // Act: Perform the action we want to test
     // Assert: Check the outcome to see if the result matches our expectations
@@ -10,55 +10,55 @@ describe("max", () => {
     expect(max(2, 1)).toBe(2);
   });
 
-  it("should return the second argument if is is greater", () => {
+  it('should return the second argument if is is greater', () => {
     expect(max(3, 5)).toBe(5);
   });
 
-  it("should return the first argument if both arguments are equal", () => {
+  it('should return the first argument if both arguments are equal', () => {
     expect(max(3, 3)).toBe(3);
   });
 });
 
-describe("fizzbuzz", () => {
-  it("should return FizzBuzz if argument is divisible by 3 and 5", () => {
-    expect(fizzBuzz(15)).toBe("FizzBuzz");
+describe('fizzbuzz', () => {
+  it('should return FizzBuzz if argument is divisible by 3 and 5', () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
   });
 
-  it("should return Fizz if argument is only divisible by 3", () => {
-    expect(fizzBuzz(3)).toBe("Fizz");
+  it('should return Fizz if argument is only divisible by 3', () => {
+    expect(fizzBuzz(3)).toBe('Fizz');
   });
 
-  it("should return Buzz if argument is only divisible by 5", () => {
-    expect(fizzBuzz(5)).toBe("Buzz");
+  it('should return Buzz if argument is only divisible by 5', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
   });
 
-  it("should return argument as a string if it is not divisible by 3 or 5", () => {
-    expect(fizzBuzz(1)).toBe("1");
+  it('should return argument as a string if it is not divisible by 3 or 5', () => {
+    expect(fizzBuzz(1)).toBe('1');
   });
 });
 
-describe("factorial", () => {
-  it("should return 1 if argument is 0", () => {
+describe('factorial', () => {
+  it('should return 1 if argument is 0', () => {
     expect(factorial(0)).toBe(1);
   });
 
-  it("should return 1 if argument is 1", () => {
+  it('should return 1 if argument is 1', () => {
     expect(factorial(1)).toBe(1);
   });
 
-  it("should return 2 if argument is 2", () => {
+  it('should return 2 if argument is 2', () => {
     expect(factorial(2)).toBe(2);
   });
 
-  it("should return 6 if argument is 3", () => {
+  it('should return 6 if argument is 3', () => {
     expect(factorial(3)).toBe(6);
   });
 
-  it("should return 24 if argument is 4", () => {
+  it('should return 24 if argument is 4', () => {
     expect(factorial(4)).toBe(24);
   });
 
-  it("should return undefined if argument is negative", () => {
+  it('should return undefined if argument is negative', () => {
     expect(factorial(-1)).toBeUndefined();
   });
 });
