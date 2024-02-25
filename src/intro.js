553639e20377fbfe614e6260ccf47ae19618a4d4
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-25 14:36:24 +0200

    chore: format code with Prettier

--- src/intro.js
+++ src/intro.js
@@ -5,9 +5,9 @@ export function max(a, b) {
 
 // Exercise
 export function fizzBuzz(n) {
-  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
-  if (n % 3 === 0) return "Fizz";
-  if (n % 5 === 0) return "Buzz";
+  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
+  if (n % 3 === 0) return 'Fizz';
+  if (n % 5 === 0) return 'Buzz';
   return n.toString();
 }
 
